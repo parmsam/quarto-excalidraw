5a1f7cb89609ff8fe2c4b76cf01c0506e916969a
parmsam <parmartsam@gmail.com>
2024-07-26 11:06:02 -0400

    setup repo

---
+++ README.md
@@ -0,0 +1,50 @@
+# Excalidraw Extension For Quarto
+
+Allows you to open up an empty [Excalidraw](https://excalidraw.com/) canvas within your RevealJS presentation. It is similar to the [built-in chalkboard plugin](https://quarto.org/docs/presentations/revealjs/presenting.html#chalkboard) in Quarto.
+
+## Installing
+
+```bash
+quarto add parmsam/excalidraw
+```
+
+This will install the extension under the `_extensions` subdirectory.
+If you're using version control, you will want to check in this directory.
+
+## Using
+
+Simply add the extension to the list of revealjs plugins like:
+
+```yaml
+title: "Excalidraw Example"
+format:
+    revealjs: default
+revealjs-plugins:
+  - excalidraw
+```
+
+Then all you have to do is click the 🎨 Artist Palette Emoji button to toggle the Excalidraw canvas. Excalidraw is running locally in your specfic slides. Click on it again to return to your slides.
+
+Alternatively, you can use the shortcut key to toggle the Excalidraw canvas. The default shortcut key is ``\` (backtick). 
+
+```yaml
+title: "Excalidraw Example"
+format:
+  revealjs:
+    chalkboard: true
+    excalidraw: 
+      button: true
+      shortcut: "`"
+revealjs-plugins:
+  - excalidraw
+```
+
+You can change the shortcut key by specifying the `shortcut` option in the configuration. You can also disable the button by setting the `button` option to `false`.
+
+## Example
+
+Here is the source code for a minimal example: [example.qmd](example.qmd).
+
+## Resources used 
+
+- https://docs.excalidraw.com/docs/@excalidraw/excalidraw/integration#browser
---
+++ _extensions/excalidraw/excalidraw.js
@@ -0,0 +1,95 @@
+function loadScript(src) {
+  return new Promise((resolve, reject) => {
+    var script = document.createElement("script");
+    script.src = src;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.head.appendChild(script);
+  });
+}
+
+async function loadDependencies() {
+  await loadScript("https://unpkg.com/react@18.2.0/umd/react.development.js");
+  await loadScript("https://unpkg.com/react-dom@18.2.0/umd/react-dom.development.js");
+  await loadScript("https://unpkg.com/@excalidraw/excalidraw/dist/excalidraw.development.js");
+  // await loadScript("./excalidraw/src/react.development.js");
+  // await loadScript("./excalidraw/src/react-dom.development.js");
+  // await loadScript("./excalidraw/src/excalidraw.development.js");
+}
+
+window.RevealExcalidraw = function () {
+  return {
+    id: "RevealExcalidraw",
+    init: async function (deck) {
+      await loadDependencies();
+
+      const config = deck.getConfig();
+      const options = config.excalidraw || {};
+
+      var settings = {};
+      console.log(settings);
+      settings.shortcut = options.shortcut || "`";
+      settings.button = options.button || false;
+
+      const excalidrawContainer = document.createElement('div');
+      excalidrawContainer.className = "drop-clip"
+      excalidrawContainer.id = 'excalidraw-container';
+      excalidrawContainer.style.display = 'none';
+      excalidrawContainer.style.position = 'fixed';
+      excalidrawContainer.style.top = '0';
+      excalidrawContainer.style.left = '0';
+      excalidrawContainer.style.width = '100%';
+      excalidrawContainer.style.height = '100%';
+      excalidrawContainer.style.backgroundColor = 'white';
+      excalidrawContainer.style.zIndex = '1000';
+      document.body.appendChild(excalidrawContainer);
+
+      if (settings.button) {
+        const toggleButton = document.createElement('div');
+        const link = document.createElement('a');
+        toggleButton.className = "drop-button";
+        toggleButton.id = "toggle-drop";
+        link.href = "#";
+        link.title = `Toggle Excalidraw (${settings.shortcut})`;
+        link.innerText = "🎨";
+
+        toggleButton.appendChild(link);
+        toggleButton.style.position = 'absolute';
+        toggleButton.style.bottom = '10px';
+        toggleButton.style.left = '50%';
+        toggleButton.style.transform = 'translateX(-50%)';
+        toggleButton.style.zIndex = '1001'; 
+        document.querySelector(".reveal").appendChild(toggleButton);
+
+        link.addEventListener('click', () => {
+          excalidrawContainer.style.display =
+            excalidrawContainer.style.display === 'none' ? 'block' : 'none';
+        });
+      }
+
+      document.addEventListener('keydown', (event) => {
+        if (event.key === settings.shortcut) {
+          excalidrawContainer.style.display =
+            excalidrawContainer.style.display === 'none' ? 'block' : 'none';
+        }
+      });
+
+      const App = () => {
+        return React.createElement(
+          React.Fragment,
+          null,
+          React.createElement(
+            "div",
+            {
+              style: { height: "100%", width: "100%" },
+            },
+            React.createElement(ExcalidrawLib.Excalidraw),
+          ),
+        );
+      };
+
+      const root = ReactDOM.createRoot(excalidrawContainer);
+      root.render(React.createElement(App));
+    },
+  };
+};
